a8bab7905efc028f92c4cb7158d5814d09bc6348
Andrew King <eurokang@gmail.com>
2014-11-15 10:54:13 +0800

    Work in progress

--- emonTH_PulseCounting/emonTH_PulseCounting.ino
+++ emonTH_PulseCounting/emonTH_PulseCounting.ino
@@ -75,16 +75,14 @@ const int UNO = 1;                                                      // Set t
 #include <RFu_JeeLib.h>                                                     // Download JeeLib: http://github.com/jcw/jeelib
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
   
-typedef struct { int power, pulse;} PayloadTX;
+typedef struct { int pulse;} PayloadTX;
 PayloadTX emontx;                                                        // neat way of packaging data for RF comms
 
 const int LEDpin = 9;                                                   //emonTH LED pin
 
 // Pulse counting settings 
 long pulseCount = 0;                                                    // Number of pulses, used to measure energy.
-unsigned long pulseTime,lastTime;                                       // Used to measure power.
-double power, elapsedWh;                                                // power and energy
-int ppwh = 1;                                                           // 1000 pulses/kwh = 1 pulse per wh - Number of pulses per wh - found or set on the meter.
+unsigned long lastTime;                                       // Used to measure power.                                               // power and energy                                                         // 1000 pulses/kwh = 1 pulse per wh - Number of pulses per wh - found or set on the meter.
 
 
 void setup() 
@@ -101,7 +99,8 @@ void setup()
   
   attachInterrupt(0, onPulse, FALLING);                                 // KWH interrupt attached to IRQ 0  = Digita 2 - hardwired to emonTx V3 terminal block 
   
-  if (UNO) wdt_enable(WDTO_8S);  
+  if (UNO) 
+    wdt_enable(WDTO_8S);  
 }
 
 void loop() 
@@ -109,8 +108,6 @@ void loop()
   emontx.pulse = pulseCount; pulseCount=0; 
   send_rf_data();  // *SEND RF DATA* - see emontx_lib
 
-  Serial.print(emontx.power);
-  Serial.print("W ");
   Serial.println(emontx.pulse);
 
   emontx_sleep(10);                                                     // sleep or delay in seconds - see emontx_lib
@@ -121,8 +118,15 @@ void loop()
 // The interrupt routine - runs each time a falling edge of a pulse is detected
 void onPulse()                  
 {
-  lastTime = pulseTime;        //used to measure time between pulses.
-  pulseTime = micros();
-  pulseCount++;                                                      //pulseCounter               
-  emontx.power = int((3600000000.0 / (pulseTime - lastTime))/ppwh);  //Calculate power
+  // Firstly, disable further interrupts here (as a kind of switch debouncing).
+  detachInterrupt(0);
+  unsigned long pulseTime = micros();
+  if ((pulseTime - lastTime) > 500000) // 0.5s
+  {
+    lastTime = pulseTime;        //used to measure time between pulses.
+    ++pulseCount;
+    Serial.println(pulseCount);
+  }
+  
+  attachInterrupt(0, onPulse, FALLING);
 }
